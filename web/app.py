14816f5a9f44b2126be67040abc7971ed5e0e276
root <root@rmlbbpapp5012.rmgv.royalmailgroup.net>
2018-04-11 21:43:16 +0100

    Added in a new webpage

--- web/app.py
+++ web/app.py
@@ -1,13 +1,14 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
+@app.route('/')
+@app.route('/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5005))
     app.run(host='0.0.0.0', port=port)
